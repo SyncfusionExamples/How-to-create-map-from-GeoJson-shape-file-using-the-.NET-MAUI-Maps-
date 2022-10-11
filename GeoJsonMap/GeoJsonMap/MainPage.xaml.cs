f1661f4293f65241ff73e88abeb1dc5b26a4f5d4
Shalini-Ashokan <shalini.ashokan@syncfusion.com>
2022-10-11 18:37:19 +0530

    Load an map using GeoJson or shape file

---
+++ GeoJsonMap/GeoJsonMap/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.Maui.Maps;
+using System.Xml;
+
+namespace GeoJsonMap;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+[ContentProperty("Source")]
+public class ImageResourceExtension : IMarkupExtension<MapSource>
+{
+    public string Source { set; get; }
+
+    public MapSource ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (String.IsNullOrEmpty(Source))
+        {
+            IXmlLineInfoProvider lineInfoProvider = serviceProvider.GetService(typeof(IXmlLineInfoProvider)) as IXmlLineInfoProvider;
+            IXmlLineInfo lineInfo = (lineInfoProvider != null) ? lineInfoProvider.XmlLineInfo : new XmlLineInfo();
+            throw new XamlParseException("ImageResourceExtension requires Source property to be set", lineInfo);
+        }
+
+        // string assemblyName = GetType().GetTypeInfo().Assembly.GetName().Name;
+        return MapSource.FromResource(Source);
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return (this as IMarkupExtension<MapSource>).ProvideValue(serviceProvider);
+    }
+}
+
+
